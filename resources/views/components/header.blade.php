71d3b1cdd9d95ce9af7e3ebe301236579684355c
YasmineAassab <yasmine.aassab@edu.uca,ma>
2021-12-26 14:14:18 +0100

    update

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<video src="{{ asset('img/entrepren.mp4'); }}" muted loop autoplay></video>
+
+            <div class="overlay"></div>
+
+            <div class="text">
+                <h2>entrepreneurship </h2> 
+                <h3>“The way to get started is to quit <br> talking and begin doing.”</h3>
+                <h4>Walt Disney</h4>
+                <a href="#">Explore</a>
+            </div>
+                <ul class="social">
+                <li><a href="#"><img src="https://i.ibb.co/x7P24fL/facebook.png"></a></li>
+                <li><a href="#"><img src="https://i.ibb.co/Wnxq2Nq/twitter.png"></a></li>
+                <li><a href="#"><img src="https://i.ibb.co/ySwtH4B/instagram.png"></a></li>
+                </ul>
